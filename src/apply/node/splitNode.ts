16f32f28d4d1c36f5ebb5729fe59bd79b2fdca9c
Ulion <ulion2002@gmail.com>
2021-04-08 06:29:33 +0800

    Get ready for slate 0.60.3, not clone original element, also try not clone unnecessary children.

--- src/apply/node/splitNode.ts
+++ src/apply/node/splitNode.ts
@@ -1,6 +1,7 @@
 import { SplitNodeOperation } from 'slate';
 import invariant from 'tiny-invariant';
-import { SharedType, SyncNode } from '../../model';
+import * as Y from 'yjs';
+import { SharedType, SyncNode, SyncElement } from '../../model';
 import { getParent } from '../../path';
 import cloneSyncElement from '../../utils/clone';
 
@@ -20,16 +21,17 @@ export default function splitNode(
   invariant(children, 'Parent of node should have children');
 
   const target = children.get(index);
-  const inject = cloneSyncElement(target);
+  const inject = new Y.Map();
   children.insert(index + 1, [inject]);
 
   Object.entries(op.properties).forEach(([key, value]) =>
     inject.set(key, value)
   );
 
-  if (SyncNode.getText(target) !== undefined) {
-    const targetText = SyncNode.getText(target);
-    const injectText = SyncNode.getText(inject);
+  const targetText = SyncNode.getText(target)
+  if (targetText !== undefined) {
+    const injectText = new Y.Text(targetText.toString().slice(op.position));
+    inject.set('text', injectText);
 
     invariant(targetText);
     invariant(injectText);
@@ -37,22 +39,25 @@ export default function splitNode(
     if (targetText.length > op.position) {
       targetText.delete(op.position, targetText.length - op.position);
     }
-
-    if (injectText !== undefined && op.position !== undefined) {
-      injectText.delete(0, op.position);
-    }
   } else {
     const targetChildren = SyncNode.getChildren(target);
-    const injectChildren = SyncNode.getChildren(inject);
+
+    const injectChildren = new Y.Array();
+    inject.set('children', injectChildren);
 
     invariant(targetChildren);
     invariant(injectChildren);
 
-    targetChildren.delete(op.position, targetChildren.length - op.position);
+    // XXX: we have to clone the array elements since yjs does not support move element from one array to the other.
+    const childElements: SyncElement[] = []
+    targetChildren.forEach((child, idx) => {
+      if (idx >= op.position) {
+        childElements.push(cloneSyncElement(child));
+      }
+    });
+    injectChildren.insert(0, childElements);
 
-    if (op.position !== undefined) {
-      injectChildren.delete(0, op.position);
-    }
+    targetChildren.delete(op.position, targetChildren.length - op.position);
   }
 
   return doc;
